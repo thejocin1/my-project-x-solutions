45eb5ee0120d5a96250d67b0be15d2a7d74ea6a8
thejocin1 <thejocin1@aol.com>
2016-01-08 01:30:27 -0500

    Update Problem7.java

--- java/Problem7.java
+++ java/Problem7.java
@@ -2,6 +2,9 @@
  * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
  * What is the 10 001st prime number?
  * 
+ * Build time on my system: 11 seconds
+ * Potential for higher efficiency
+ * 
  * @author Thejocin1
  */
 public class Problem7 {
