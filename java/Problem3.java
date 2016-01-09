d96ab3d8a4267f81f51b3c20e39d4fd660aac8e6
thejocin1 <thejocin1@aol.com>
2016-01-09 00:36:01 -0500

    Update Problem3.java
    Accidentally replaced Problem 3 with Problem 7. Corrected this mistake.

--- java/Problem3.java
+++ java/Problem3.java
@@ -1,9 +1,9 @@
 /**
- * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
- * What is the 10 001st prime number?
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * What is the largest prime factor of the number 600851475143 ?
  * 
- * Build time on my system: 1 second
- * Potential for higher efficiency
+ * Build time on my system: Way too long for practicality
+ * Potential for higher efficiency and readability
  * 
  * @author thejocin1
  */
@@ -14,33 +14,40 @@ public class Problem3 {
      */
     public static void main(String[] args) {
 
-        int iteration = 1;
-        int limit = 10001;
-        int number = 2;
+        boolean found = false;
+        long number = 600_851_475_143L;
+        long factor = number / 2;
         
-        while (iteration <= limit)
+        while (found == false)
         {
-            int checkPrime = 2;
-            boolean isPrime = true;
-            
-            while (checkPrime <= number / 2 && isPrime == true)
+            if (number % factor == 0)
             {
-                if (number % checkPrime == 0)
+                long checkPrime = 2;
+                boolean isPrime = true;
+
+                while (checkPrime <= factor / 2 && isPrime == true)
                 {
-                   isPrime = false;
+                    if (factor % checkPrime == 0)
+                    {
+                       isPrime = false;
+                    }
+                    
+                    checkPrime++;
                 }
-                checkPrime++;
+
+                if (isPrime == true)
+                {
+                    found = true;
+                }
             }
             
-            if (isPrime == true)
+            if (found == false)
             {
-                iteration++;
+                factor--;     
             }
-            
-            number++;
         }
         
-        System.out.println("The " + (iteration - 1) + "th prime number is " + (number - 1));
+        System.out.println("The largest prime factor of the number " + number + " is " + factor);
     }
     
 }
